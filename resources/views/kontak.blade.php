d69d5b02fde4e87eff9cea435a4fe2a76fed4b33
hihwibu <gerijayakusumah@gmail.com>
2022-02-14 10:37:19 +0700

    first commit

---
+++ resources/views/kontak.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('judul_halaman', 'Halaman Home')
+
+@section('konten')
+
+<p>Ini Adalah Halam kontak</P>
+<p>Selamat Datang !</P>
+
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h2>WELCOME TO WEBSITE RPL</h2>
+        <nav>
+            <a href="/rpl/home">HOME</a>
+            |
+            <a href="/rpl/tentang">TENTANG</a>
+            |
+            <a href="/rpl/kontak">KONTAK</a>
+</nav>
+</header>
+
+</hr>
+</br>
+</br>
+
+<h3> @yield('judul_halaman')</h3>
+
+@yield('konten')
+<br/>
+<br/>
+<hr/>
+<footer>
+    <p>&copy; <a href="#">www.mywebsite.com</a> 2022</p>
+</footer>
+</body>
+</html> 
